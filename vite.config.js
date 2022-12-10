ae2e4b1d5b661896b6a836dd1071051dbf745df1
Nik Lub <nlubyanoy@fluxergy.com>
2022-12-10 14:19:51 -0800

    Added react icon for vite plugin

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import reactIcons from 'vite-plugin-react-icons';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-    mode: "development",
+    plugins: [react(), viteTsconfigPaths(), svgrPlugin(), reactIcons()],
     build: {
         minify: false,
     }
