4f38993a795f96a0b9a4140cbb0dddab56b14b80
Nik Lub <nlubyanoy@fluxergy.com>
2023-01-28 09:00:50 -0800

    Removed unused pages from import

--- src/routes/paths.ts
+++ src/routes/paths.ts
@@ -25,22 +25,13 @@ export const PATH_AUTH = {
 export const PATH_PAGE = {
     comingSoon: '/coming-soon',
     maintenance: '/maintenance',
-    pricing: '/pricing',
-    payment: '/payment',
-    about: '/about-us',
-    contact: '/contact-us',
-    faqs: '/faqs',
     page403: '/403',
     page404: '/404',
     page500: '/500',
-    components: '/components',
 };
 
 export const PATH_DASHBOARD = {
     root: ROOTS_DASHBOARD,
-    kanban: path(ROOTS_DASHBOARD, '/kanban'),
-    calendar: path(ROOTS_DASHBOARD, '/calendar'),
-    fileManager: path(ROOTS_DASHBOARD, '/files-manager'),
     permissionDenied: path(ROOTS_DASHBOARD, '/permission-denied'),
     blank: path(ROOTS_DASHBOARD, '/blank'),
     general: {
@@ -58,33 +49,11 @@ export const PATH_DASHBOARD = {
         // vips: path(ROOTS_DASHBOARD, '/vips'),
         rvs: path(ROOTS_DASHBOARD, '/rvs'),
         tents: path(ROOTS_DASHBOARD, '/tents'),
-        app: path(ROOTS_DASHBOARD, '/app'),
-        ecommerce: path(ROOTS_DASHBOARD, '/ecommerce'),
-        analytics: path(ROOTS_DASHBOARD, '/analytics'),
-        banking: path(ROOTS_DASHBOARD, '/banking'),
-        booking: path(ROOTS_DASHBOARD, '/booking'),
-        file: path(ROOTS_DASHBOARD, '/file'),
         guidelines: path(ROOTS_DASHBOARD, '/guidelines'),
         camp_guidelines: (campId: number) => path(ROOTS_DASHBOARD, `/guidelines/${campId}`)
     },
-    mail: {
-        root: path(ROOTS_DASHBOARD, '/mail'),
-        all: path(ROOTS_DASHBOARD, '/mail/all'),
-    },
-    chat: {
-        root: path(ROOTS_DASHBOARD, '/chat'),
-        new: path(ROOTS_DASHBOARD, '/chat/new'),
-        view: (name: string) => path(ROOTS_DASHBOARD, `/chat/${name}`),
-    },
     user: {
-        root: path(ROOTS_DASHBOARD, '/user'),
-        new: path(ROOTS_DASHBOARD, '/user/new'),
-        list: path(ROOTS_DASHBOARD, '/user/list'),
-        cards: path(ROOTS_DASHBOARD, '/user/cards'),
-        profile: path(ROOTS_DASHBOARD, '/user/profile'),
         account: path(ROOTS_DASHBOARD, '/user/account'),
-        edit: (name: string) => path(ROOTS_DASHBOARD, `/user/${name}/edit`),
-        demoEdit: path(ROOTS_DASHBOARD, `/user/reece-chung/edit`),
     },
     eCommerce: {
         root: path(ROOTS_DASHBOARD, '/e-commerce'),
@@ -97,13 +66,7 @@ export const PATH_DASHBOARD = {
         demoEdit: path(ROOTS_DASHBOARD, '/e-commerce/product/nike-blazer-low-77-vintage/edit'),
         demoView: path(ROOTS_DASHBOARD, '/e-commerce/product/nike-air-force-1-ndestrukt'),
     },
-    blog: {
-        root: path(ROOTS_DASHBOARD, '/blog'),
-        posts: path(ROOTS_DASHBOARD, '/blog/posts'),
-        new: path(ROOTS_DASHBOARD, '/blog/new'),
-        view: (title: string) => path(ROOTS_DASHBOARD, `/blog/post/${title}`),
-        demoView: path(ROOTS_DASHBOARD, '/blog/post/apply-these-7-secret-techniques-to-improve-event'),
-    },
+
 };
 
 export const PATH_DOCS = {
