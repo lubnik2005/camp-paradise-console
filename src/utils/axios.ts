53b614e764a5c9c69569895e86b9143620f9a46e
Nik Lub <nlubyanoy@fluxergy.com>
2023-01-27 22:21:25 -0800

    log base url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,10 +5,12 @@ import { HOST_API_KEY } from '../config-global';
 // ----------------------------------------------------------------------
 
 const axiosInstance = axios.create({ baseURL: HOST_API_KEY });
+console.log('BASEURL');
+console.log(HOST_API_KEY);
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+    (response) => response,
+    (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
 );
 
 export default axiosInstance;
