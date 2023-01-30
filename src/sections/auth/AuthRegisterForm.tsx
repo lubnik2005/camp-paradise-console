6d9a117398f7d533a3c22803f30aba6ced110e9d
Nik Lub <nlubyanoy@fluxergy.com>
2023-01-30 13:07:09 -0800

    change name of genders

--- src/sections/auth/AuthRegisterForm.tsx
+++ src/sections/auth/AuthRegisterForm.tsx
@@ -15,110 +15,110 @@ import FormProvider, { RHFTextField, RHFRadioGroup } from '../../components/hook
 // ----------------------------------------------------------------------
 
 type FormValuesProps = {
-  email: string;
-  password: string;
-  gender: string;
-  firstName: string;
-  lastName: string;
-  afterSubmit?: string;
+    email: string;
+    password: string;
+    gender: string;
+    firstName: string;
+    lastName: string;
+    afterSubmit?: string;
 };
 
 export default function AuthRegisterForm() {
-  const { register } = useAuthContext();
+    const { register } = useAuthContext();
 
-  const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-  const RegisterSchema = Yup.object().shape({
-    firstName: Yup.string().required('First name required'),
-    lastName: Yup.string().required('Last name required'),
-    gender: Yup.string().required('Gender is required'),
-    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
-    password: Yup.string().required('Password is required'),
-  });
+    const RegisterSchema = Yup.object().shape({
+        firstName: Yup.string().required('First name required'),
+        lastName: Yup.string().required('Last name required'),
+        gender: Yup.string().required('Gender is required'),
+        email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+        password: Yup.string().required('Password is required'),
+    });
 
-  const defaultValues = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    gender: ''
-  };
+    const defaultValues = {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        gender: ''
+    };
 
-  const methods = useForm<FormValuesProps>({
-    resolver: yupResolver(RegisterSchema),
-    defaultValues,
-  });
+    const methods = useForm<FormValuesProps>({
+        resolver: yupResolver(RegisterSchema),
+        defaultValues,
+    });
 
-  const {
-    reset,
-    setError,
-    handleSubmit,
-    formState: { errors, isSubmitting, isSubmitSuccessful },
-  } = methods;
+    const {
+        reset,
+        setError,
+        handleSubmit,
+        formState: { errors, isSubmitting, isSubmitSuccessful },
+    } = methods;
 
-  const onSubmit = async (data: FormValuesProps) => {
-    try {
-      if (register) {
-        await register(data.email, data.password, data.firstName, data.lastName, data.gender);
-      }
-    } catch (error) {
-      console.error(error);
-      reset();
-      setError('afterSubmit', {
-        ...error,
-        message: error.message,
-      });
-    }
-  };
+    const onSubmit = async (data: FormValuesProps) => {
+        try {
+            if (register) {
+                await register(data.email, data.password, data.firstName, data.lastName, data.gender);
+            }
+        } catch (error) {
+            console.error(error);
+            reset();
+            setError('afterSubmit', {
+                ...error,
+                message: error.message,
+            });
+        }
+    };
 
-  return (
-    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={2.5}>
-        {!!errors.afterSubmit && <Alert severity="error">{errors.afterSubmit.message}</Alert>}
+    return (
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={2.5}>
+                {!!errors.afterSubmit && <Alert severity="error">{errors.afterSubmit.message}</Alert>}
 
-        <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
-          <RHFTextField name="firstName" label="First name" />
-          <RHFTextField name="lastName" label="Last name" />
-        </Stack>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                    <RHFTextField name="firstName" label="First name" />
+                    <RHFTextField name="lastName" label="Last name" />
+                </Stack>
 
-        <RHFRadioGroup name="gender" options={[{label: 'Guy', value: 'm'}, {label: 'Gal', value: 'f'}]} label="Gender" row/>
+                <RHFRadioGroup name="gender" options={[{ label: 'Male', value: 'Female' }, { label: 'Gal', value: 'f' }]} label="Gender" row />
 
-        <RHFTextField name="email" label="Email address" />
+                <RHFTextField name="email" label="Email address" />
 
-        <RHFTextField
-          name="password"
-          label="Password"
-          type={showPassword ? 'text' : 'password'}
-          InputProps={{
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
-                  <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
-                </IconButton>
-              </InputAdornment>
-            ),
-          }}
-        />
+                <RHFTextField
+                    name="password"
+                    label="Password"
+                    type={showPassword ? 'text' : 'password'}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton onClick={() => setShowPassword(!showPassword)} edge="end">
+                                    <Iconify icon={showPassword ? 'eva:eye-fill' : 'eva:eye-off-fill'} />
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                />
 
-        <LoadingButton
-          fullWidth
-          color="inherit"
-          size="large"
-          type="submit"
-          variant="contained"
-          loading={isSubmitSuccessful || isSubmitting}
-          sx={{
-            bgcolor: 'text.primary',
-            color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
-            '&:hover': {
-              bgcolor: 'text.primary',
-              color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
-            },
-          }}
-        >
-          Create account
-        </LoadingButton>
-      </Stack>
-    </FormProvider>
-  );
+                <LoadingButton
+                    fullWidth
+                    color="inherit"
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                    loading={isSubmitSuccessful || isSubmitting}
+                    sx={{
+                        bgcolor: 'text.primary',
+                        color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
+                        '&:hover': {
+                            bgcolor: 'text.primary',
+                            color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
+                        },
+                    }}
+                >
+                    Create account
+                </LoadingButton>
+            </Stack>
+        </FormProvider>
+    );
 }
