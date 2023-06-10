b8b3974ce9676d71e24777be2010d9562f5cf47f
Nik Lub <nlubyanoy@fluxergy.com>
2023-06-10 08:22:56 -0700

    The verify needs the token in the get request lol

--- src/pages/auth/VerifyJwtPage.tsx
+++ src/pages/auth/VerifyJwtPage.tsx
@@ -16,103 +16,98 @@ import { PATH_AUTH } from '../../routes/paths';
 // ----------------------------------------------------------------------
 
 export default function VerifyJwtPage() {
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get('token');
+  const [verified, setVerified] = useState(false);
+  const [success, setSuccess] = useState(false);
+  console.log(token);
 
+  const verify = useCallback(async () => {
+    try {
+      const response = await axios.get(`/verify?token=${token}`);
+      console.log(response);
+      setVerified(true);
+      if (response.status === 200) setSuccess(true);
+    } catch (error) {
+      console.log(error);
+      setVerified(true);
+      setSuccess(false);
+      console.log({ error: { error: ['Cannot connect to api.'] } });
+    }
+  }, [token]);
 
-    const params = new URLSearchParams(window.location.search);
-    const token = params.get('token')
-    const [verified, setVerified] = useState(false)
-    const [success, setSuccess] = useState(false)
-    console.log(token);
+  useEffect(() => {
+    const fetchData = async () => verify();
+    fetchData();
+  }, [verify]);
 
-    const verify = useCallback(async () => {
-        try {
-            const response = await axios.get("/verify");
-            console.log(response);
-            setVerified(true);
-            if (response.status === 200) setSuccess(true);
-        } catch (error) {
-            console.log(error);
-            setVerified(true);
-            setSuccess(false);
-            console.log({ error: { error: ['Cannot connect to api.'] } });
-        }
-    }, [token]);
+  if (!verified) return <CircularProgress />;
 
-    useEffect(() => {
-        const fetchData = async () => verify();
-        fetchData();
-    }, [verify]);
+  return success ? (
+    <>
+      <Helmet>
+        <title> Verify Code | Camp Paradise</title>
+      </Helmet>
 
-    if (!verified) return <CircularProgress />
+      <EmailInboxIcon sx={{ mb: 5, height: 96 }} />
 
+      <Typography variant="h3" paragraph>
+        E-mail verified!
+      </Typography>
 
-    return (
-        success ?
-            <>
-                <Helmet>
-                    <title> Verify Code | Camp Paradise</title>
-                </Helmet>
+      <Typography sx={{ color: 'text.secondary', mb: 5 }}>Your email has been verified.</Typography>
 
-                <EmailInboxIcon sx={{ mb: 5, height: 96 }} />
+      <Link
+        component={RouterLink}
+        to={PATH_AUTH.login}
+        color="inherit"
+        variant="subtitle2"
+        sx={{
+          mx: 'auto',
+          alignItems: 'center',
+          display: 'inline-flex',
+        }}
+      >
+        <Iconify icon="eva:chevron-left-fill" width={16} />
+        Return to sign in
+      </Link>
+    </>
+  ) : (
+    <>
+      <Helmet>
+        <title> Verify Code | Camp Paradise</title>
+      </Helmet>
 
-                <Typography variant="h3" paragraph>
-                    E-mail verified!
-                </Typography>
+      <EmailInboxIcon sx={{ mb: 5, height: 96 }} />
 
+      <Typography variant="h3" paragraph>
+        Failed to verify your email!
+      </Typography>
 
-                <Typography sx={{ color: 'text.secondary', mb: 5 }}>
-                    Your email has been verified.
-                </Typography>
+      <Typography sx={{ color: 'text.secondary', mb: 5 }}>Please check yoor E-mail.</Typography>
 
-                <Link
-                    component={RouterLink}
-                    to={PATH_AUTH.login}
-                    color="inherit"
-                    variant="subtitle2"
-                    sx={{
-                        mx: 'auto',
-                        alignItems: 'center',
-                        display: 'inline-flex',
-                    }}
-                >
-                    <Iconify icon="eva:chevron-left-fill" width={16} />
-                    Return to sign in
-                </Link>
-            </> :
-            <>
-                <Helmet>
-                    <title> Verify Code | Camp Paradise</title>
-                </Helmet>
+      <Typography variant="body2" sx={{ my: 3 }}>
+        Didn&apos;t receive an E-mail? &nbsp;
+        <RouterLink to={PATH_AUTH.resend}>
+          {' '}
+          <Link variant="subtitle2">Resend E-mail</Link>
+        </RouterLink>
+      </Typography>
 
-                <EmailInboxIcon sx={{ mb: 5, height: 96 }} />
-
-                <Typography variant="h3" paragraph>
-                    Failed to verify your email!
-                </Typography>
-
-                <Typography sx={{ color: 'text.secondary', mb: 5 }}>
-                    Please check yoor E-mail.
-                </Typography>
-
-                <Typography variant="body2" sx={{ my: 3 }}>
-                    Didn&apos;t receive an E-mail? &nbsp;
-                    <RouterLink to={PATH_AUTH.resend}> <Link variant="subtitle2">Resend E-mail</Link></RouterLink>
-                </Typography>
-
-                <Link
-                    component={RouterLink}
-                    to={PATH_AUTH.login}
-                    color="inherit"
-                    variant="subtitle2"
-                    sx={{
-                        mx: 'auto',
-                        alignItems: 'center',
-                        display: 'inline-flex',
-                    }}
-                >
-                    <Iconify icon="eva:chevron-left-fill" width={16} />
-                    Return to sign in
-                </Link>
-            </>
-    );
+      <Link
+        component={RouterLink}
+        to={PATH_AUTH.login}
+        color="inherit"
+        variant="subtitle2"
+        sx={{
+          mx: 'auto',
+          alignItems: 'center',
+          display: 'inline-flex',
+        }}
+      >
+        <Iconify icon="eva:chevron-left-fill" width={16} />
+        Return to sign in
+      </Link>
+    </>
+  );
 }
