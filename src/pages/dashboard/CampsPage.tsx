27e1113ad00526cbbafdc7b3f98f499aff4d136f
Nik Lub <nlubyanoy@fluxergy.com>
2023-06-09 21:44:57 -0700

    Fixed some styling

--- src/pages/dashboard/CampsPage.tsx
+++ src/pages/dashboard/CampsPage.tsx
@@ -11,16 +11,14 @@ import { PATH_DASHBOARD } from '../../routes/paths';
 // auth
 import { useAuthContext } from '../../auth/useAuthContext';
 // axios
-import axios from "../../utils/axios";
+import axios from '../../utils/axios';
 // _mock_
 import AXIOS from 'axios';
 
 // components
 import { useSettingsContext } from '../../components/settings';
 // sections
-import {
-    AppWelcome,
-} from '../../sections/@dashboard/general/app';
+import { AppWelcome } from '../../sections/@dashboard/general/app';
 // assets
 import { SeoIllustration } from '../../assets/illustrations';
 // storage
@@ -29,101 +27,127 @@ import { Camp } from '../../@types/camp';
 // ----------------------------------------------------------------------
 
 export default function CampsPage() {
-    const { user } = useAuthContext();
+  const { user } = useAuthContext();
 
-    const storageAvailable = localStorageAvailable();
+  const storageAvailable = localStorageAvailable();
 
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const { themeStretch } = useSettingsContext();
+  const { themeStretch } = useSettingsContext();
 
-    const [camps, setCamps] = useState<Array<Camp> | undefined>();
-    const [upcomingCamps, setUpcomingCamps] = useState<Array<Camp> | undefined>();
-    const [previousCamps, setPreviousCamps] = useState<Array<Camp> | undefined>();
+  const [camps, setCamps] = useState<Array<Camp> | undefined>();
+  const [upcomingCamps, setUpcomingCamps] = useState<Array<Camp> | undefined>();
+  const [previousCamps, setPreviousCamps] = useState<Array<Camp> | undefined>();
 
-    type CampAgreement = {
-        campId: string,
-        acceptedOn: string | null,
-        userId: string | null
-    }
+  type CampAgreement = {
+    campId: string;
+    acceptedOn: string | null;
+    userId: string | null;
+  };
 
-    let guidelinesAgreements: CampAgreement[] = [];
+  let guidelinesAgreements: CampAgreement[] = [];
+  try {
+    guidelinesAgreements = JSON.parse(
+      storageAvailable ? localStorage.getItem('guidelineAgreements') ?? '[]' : '[]'
+    );
+  } catch {}
+
+  const getCamps = useCallback(async () => {
     try {
-        guidelinesAgreements = JSON.parse(storageAvailable ? localStorage.getItem('guidelineAgreements') ?? '[]' : '[]');
-    } catch {
+      const { data: events } = await axios.get('/current_events');
+      const { data: upcoming } = await axios.get('/upcoming_events');
+      const { data: previous } = await axios.get('/previous_events');
+      setCamps(events);
+      setUpcomingCamps(upcoming);
+      setPreviousCamps(previous);
+    } catch (error) {
+      console.log(error);
     }
+  }, [storageAvailable]);
 
-    const getCamps = useCallback(async () => {
-        try {
-            const { data: events } = await axios.get("/events");
-            const { data: upcoming } = await axios.get("/upcoming_events");
-            const { data: previous } = await axios.get("/previous_events");
-            setCamps(events);
-            setUpcomingCamps(upcoming);
-            setPreviousCamps(previous);
-        } catch (error) {
-            console.log(error);
-        }
-    }, [storageAvailable]);
+  useEffect(() => {
+    getCamps();
+  }, [getCamps]);
 
-    useEffect(() => {
-        getCamps();
-    }, [getCamps]);
-
-
-    const DisplayButton = ({ camp }: { camp: Camp }) => {
-        if (!guidelinesAgreements.find((agreement: CampAgreement) => agreement.campId === camp.id.toString() && !!agreement.acceptedOn && agreement.userId === user?.id.toString())) {
-            return <Button component={RouterLink} to={PATH_DASHBOARD.general.camp_guidelines(camp.id)} variant="contained">
-                Read and Accept Guidelines To Register
-            </Button>
-        }
-        return camp.reservations.length < 1 ?
-            <Button component={RouterLink} to={PATH_DASHBOARD.general.buildings(camp.id)} variant="contained">Register</Button>
-            : <Button component={RouterLink} to={PATH_DASHBOARD.general.buildings(camp.id)} variant="contained">View</Button>;
+  const DisplayButton = ({ camp }: { camp: Camp }) => {
+    if (
+      !guidelinesAgreements.find(
+        (agreement: CampAgreement) =>
+          agreement.campId === camp.id.toString() &&
+          !!agreement.acceptedOn &&
+          agreement.userId === user?.id.toString()
+      )
+    ) {
+      return (
+        <Button
+          component={RouterLink}
+          to={PATH_DASHBOARD.general.camp_guidelines(camp.id)}
+          variant="contained"
+        >
+          Read and Accept Guidelines To Register
+        </Button>
+      );
     }
+    return camp.reservations?.length < 1 ? (
+      <Button
+        component={RouterLink}
+        to={PATH_DASHBOARD.general.buildings(camp.id)}
+        variant="contained"
+      >
+        Register
+      </Button>
+    ) : (
+      <Button
+        component={RouterLink}
+        to={PATH_DASHBOARD.general.buildings(camp.id)}
+        variant="contained"
+      >
+        View
+      </Button>
+    );
+  };
 
-    return (camps ?
-        <>
-            <Helmet>
-                <title>Camps | Camp Paradise</title>
-            </Helmet>
-            <Container maxWidth={themeStretch ? false : 'xl'}>
-                <Typography paragraph variant="h4" sx={{ whiteSpace: 'pre-line' }}>
-                    Current Camps
-                </Typography>
-                <Grid container spacing={3}>
-                    {camps != null ? camps.map((camp: Camp) => <Grid
-                        key={`upcoming - camp - ${camp.id}`}
-                        item
-                        xs={12}
-                        md={12} >
-                        <AppWelcome
-                            style={{ padding: '3em' }}
-                            title={camp.name}
-                            description={`${camp.start_on.slice(0, 10).replace(/-/g, '/')} — ${camp.end_on.slice(0, 10).replace(/-/g, '/')}`}
-                            // img={
-                            //     <SeoIllustration
-                            //         sx={{
-                            //             p: 3,
-                            //             width: 360,
-                            //         }}
-                            //     />
-                            // }
-                            action={<DisplayButton camp={camp} />}
-                        />
-                    </Grid>) : <LoadingScreen />}
-                    {camps !== null && camps.length === 0 ? <Grid item
-                        xs={12}
-                        md={12} >
-                        <Typography variant="h3" >No Camps Yet</Typography>
-                    </Grid> : null}
-                </Grid>
-                <Typography paragraph variant="h4" sx={{ whiteSpace: 'pre-line' }}>
-                    Upcoming Camps
-                </Typography>
-                <Typography paragraph variant="h4" sx={{ whiteSpace: 'pre-line' }}>
-                    Previous Camps
-                </Typography>
-            </Container>
-        </> : null);
+  return camps ? (
+    <>
+      <Helmet>
+        <title>Camps | Camp Paradise</title>
+      </Helmet>
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Typography paragraph variant="h4" sx={{ whiteSpace: 'pre-line' }}>
+          Current Camps
+        </Typography>
+        <Grid container spacing={3}>
+          {camps != null ? (
+            camps.map((camp: Camp) => (
+              <Grid key={`current-camp-${camp.id}`} item xs={12} md={12}>
+                <AppWelcome
+                  style={{ padding: '3em' }}
+                  title={camp.name}
+                  description={`${camp.start_on.slice(0, 10).replace(/-/g, '/')} — ${camp.end_on
+                    .slice(0, 10)
+                    .replace(/-/g, '/')}`}
+                  // img={
+                  //     <SeoIllustration
+                  //         sx={{
+                  //             p: 3,
+                  //             width: 360,
+                  //         }}
+                  //     />
+                  // }
+                  action={<DisplayButton camp={camp} />}
+                />
+              </Grid>
+            ))
+          ) : (
+            <LoadingScreen />
+          )}
+          {camps !== null && camps.length === 0 ? (
+            <Grid item xs={12} md={12}>
+              <Typography variant="h3">No Camps Yet</Typography>
+            </Grid>
+          ) : null}
+        </Grid>
+      </Container>
+    </>
+  ) : null;
 }
--- src/sections/@dashboard/general/TemporaryRoomsPage.tsx
+++ src/sections/@dashboard/general/TemporaryRoomsPage.tsx
@@ -3,17 +3,16 @@ import { Helmet } from 'react-helmet-async';
 import { useEffect, useState, useCallback } from 'react';
 import LoadingScreen from 'src/components/loading-screen';
 import {
-    Container,
-    Grid,
-    Box,
-    List,
-    Divider,
-    ListItem,
-    ListItemText,
-    ListItemIcon,
-    ListItemButton,
-    Typography
-
+  Container,
+  Grid,
+  Box,
+  List,
+  Divider,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  ListItemButton,
+  Typography,
 } from '@mui/material';
 import { Link as RouterLink, useParams, useLocation, useNavigate } from 'react-router-dom';
 // utils
@@ -26,123 +25,141 @@ import { PATH_DASHBOARD } from '../../../routes/paths';
 import { useAuthContext } from '../../../auth/useAuthContext';
 // _mock_
 
-
 // components
 import { useSettingsContext } from '../../../components/settings';
 // sections
-import {
-    AppWelcome,
-} from './app';
+import { AppWelcome } from './app';
 
 import { Camp } from '../../../@types/camp';
 import CustomBreadcrumbs from '../../../components/custom-breadcrumbs';
-
 
 // assets
 
 // ----------------------------------------------------------------------
 
-export default function TemporaryRoomsPage({ title, query }: { title: string, query: string }) {
-    const { user } = useAuthContext();
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { campId } = useParams();
-    const [rooms, setRooms] = useState<Array<Room> | undefined>();
-    const [camp, setCamp] = useState<Camp | undefined>();
+export default function TemporaryRoomsPage({ title, query }: { title: string; query: string }) {
+  const { user } = useAuthContext();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { campId } = useParams();
+  const [rooms, setRooms] = useState<Array<Room> | undefined>();
+  const [camp, setCamp] = useState<Camp | undefined>();
 
-    const getRooms = useCallback(async () => {
-        try {
-            const response = await axios.get(`rooms&event_id=${campId}`)
-            console.log(response);
-            setRooms(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    }, [campId]);
-
-    const getCamps = useCallback(async () => {
-        try {
-            const { data } = await axios.get("/events")
-            const find = data.find((c: Camp) => c.id === parseInt(campId ?? '', 10));
-            setCamp(find);
-            if (!find) navigate(PATH_DASHBOARD.general.camps);
-        } catch (error) {
-            console.log(error);
-        }
-        if (!campId) navigate(PATH_DASHBOARD.general.camps)
-    }, [campId, navigate]);
-
-    interface Room {
-        id: number,
-        type: string,
-        name: string,
+  const getRooms = useCallback(async () => {
+    try {
+      const response = await axios.get(`rooms?event_id=${campId}`);
+      console.log(response);
+      setRooms(response.data);
+    } catch (error) {
+      console.log(error);
     }
+  }, [campId]);
 
-    const { themeStretch } = useSettingsContext();
-
-    useEffect(() => {
-        getRooms();
-    }, [getRooms, campId]);
-
-    useEffect(() => {
-        getCamps();
-    }, [getCamps, campId]);
-
-
+  const getCamps = useCallback(async () => {
+    try {
+      const { data } = await axios.get('/events');
+      const find = data.find((c: Camp) => c.id === parseInt(campId ?? '', 10));
+      setCamp(find);
+      if (!find) navigate(PATH_DASHBOARD.general.camps);
+    } catch (error) {
+      console.log(error);
+    }
     if (!campId) navigate(PATH_DASHBOARD.general.camps);
-    return (
-        <>
-            <Helmet>
-                <title> {title} | Camp Paradise</title>
-            </Helmet>
+  }, [campId, navigate]);
 
-            <Container maxWidth={themeStretch ? false : 'xl'}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={12} >
-                        {camp ? <AppWelcome
-                            title={camp.name}
-                            description={`${camp.start_on.slice(0, 10).replace(/-/g, '/')} — ${camp.end_on.slice(0, 10).replace(/-/g, '/')}`}
-                            img={<Box sx={{ display: { xs: 'none', md: 'inherit' } }}>
-                                <img
-                                    alt="undraw_into_the_night_vumi"
-                                    style={{
-                                        padding: '1.2em',
-                                        width: 360,
-                                    }}
-                                    src="/assets/undraw_into_the_night_vumi.svg" />
-                            </Box>}
-                            action={<CustomBreadcrumbs
-                                links={[
-                                    { name: 'All camps', href: PATH_DASHBOARD.general.camps },
-                                    { name: camp.name, href: PATH_DASHBOARD.general.buildings(camp.id) },
-                                    { name: title, }
-                                ]}
-                            />}
-                        /> : <LoadingScreen />}
-                    </Grid>
-                    <Grid item xs={12} md={12}>
-                        <Box sx={{ width: '100%' }}>
-                            <nav aria-label="main mailbox folders">
-                                {camp && rooms !== undefined ? rooms.filter((room: Room) => room.type === query).map((room: Room) =>
-                                    <List key={`room-${room.id}`}>
-                                        <ListItem>
-                                            <ListItemButton component={RouterLink} to={PATH_DASHBOARD.general.cots(camp.id, room.id)} >
-                                                <ListItemIcon />
-                                                <ListItemText primary={room.name} />
-                                            </ListItemButton>
-                                        </ListItem>
-                                        <Divider variant="inset" component="li" />
-                                    </List>) : <LoadingScreen />}
-                                {camp && rooms !== undefined && rooms.filter((room: Room) => room.type === query).length === 0 ? <Grid item
-                                    xs={12}
-                                    md={12} >
-                                    <Typography variant="h4" >Not Available</Typography>
-                                </Grid> : null}
-                            </nav>
-                        </Box>
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    );
+  interface Room {
+    id: number;
+    type: string;
+    name: string;
+  }
+
+  const { themeStretch } = useSettingsContext();
+
+  useEffect(() => {
+    getRooms();
+  }, [getRooms, campId]);
+
+  useEffect(() => {
+    getCamps();
+  }, [getCamps, campId]);
+
+  if (!campId) navigate(PATH_DASHBOARD.general.camps);
+  return (
+    <>
+      <Helmet>
+        <title> {title} | Camp Paradise</title>
+      </Helmet>
+
+      <Container maxWidth={themeStretch ? false : 'xl'}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={12}>
+            {camp ? (
+              <AppWelcome
+                title={camp.name}
+                description={`${camp.start_on.slice(0, 10).replace(/-/g, '/')} — ${camp.end_on
+                  .slice(0, 10)
+                  .replace(/-/g, '/')}`}
+                img={
+                  <Box sx={{ display: { xs: 'none', md: 'inherit' } }}>
+                    <img
+                      alt="undraw_into_the_night_vumi"
+                      style={{
+                        padding: '1.2em',
+                        width: 360,
+                      }}
+                      src="/assets/undraw_into_the_night_vumi.svg"
+                    />
+                  </Box>
+                }
+                action={
+                  <CustomBreadcrumbs
+                    links={[
+                      { name: 'All camps', href: PATH_DASHBOARD.general.camps },
+                      { name: camp.name, href: PATH_DASHBOARD.general.buildings(camp.id) },
+                      { name: title },
+                    ]}
+                  />
+                }
+              />
+            ) : (
+              <LoadingScreen />
+            )}
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <Box sx={{ width: '100%' }}>
+              <nav aria-label="main mailbox folders">
+                {camp && rooms !== undefined ? (
+                  rooms
+                    .filter((room: Room) => room.type === query)
+                    .map((room: Room) => (
+                      <List key={`room-${room.id}`}>
+                        <ListItem>
+                          <ListItemButton
+                            component={RouterLink}
+                            to={PATH_DASHBOARD.general.cots(camp.id, room.id)}
+                          >
+                            <ListItemIcon />
+                            <ListItemText primary={room.name} />
+                          </ListItemButton>
+                        </ListItem>
+                        <Divider variant="inset" component="li" />
+                      </List>
+                    ))
+                ) : (
+                  <LoadingScreen />
+                )}
+                {camp &&
+                rooms !== undefined &&
+                rooms.filter((room: Room) => room.type === query).length === 0 ? (
+                  <Grid item xs={12} md={12}>
+                    <Typography variant="h4">Not Available</Typography>
+                  </Grid>
+                ) : null}
+              </nav>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
